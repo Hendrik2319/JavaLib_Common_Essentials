001d3210a17022b7d06bd1b80c45ed11bf1fe322
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-07-06 17:34:46 +0200

    feat: moved ScrollPosition from project SnowRunner
    [SnowRunner]
    net.schwarzbaer.java.games.snowrunner.SnowRunner.ScrollValues
    
    into
    
    [JavaLib_Common_Essentials]
    net.schwarzbaer.java.lib.gui.ScrollPosition

---
+++ src/net/schwarzbaer/java/lib/gui/ScrollPosition.java
@@ -0,0 +1,65 @@
+package net.schwarzbaer.java.lib.gui;
+
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+public record ScrollPosition(int min, int max, int ext, int val)
+{
+	public enum ScrollBarType {
+		Vertical, Horizontal
+	}
+	
+	public static JScrollBar getScrollBar(JScrollPane scrollPane, ScrollBarType scrollBarType)
+	{
+		if (scrollBarType!=null && scrollPane!=null)
+			switch (scrollBarType)
+			{
+				case Horizontal: return scrollPane.getHorizontalScrollBar();
+				case Vertical  : return scrollPane.getVerticalScrollBar();
+			}
+		return null;
+	}
+
+	public static ScrollPosition get(JScrollBar scrollBar)
+	{
+		if (scrollBar==null) return null;
+		int min = scrollBar.getMinimum();
+		int max = scrollBar.getMaximum();
+		int ext = scrollBar.getVisibleAmount();
+		int val = scrollBar.getValue();
+		return new ScrollPosition(min, max, ext, val);
+	}
+
+	public static ScrollPosition get(JScrollPane scrollPane, ScrollBarType scrollBarType)
+	{
+		return get(getScrollBar(scrollPane, scrollBarType));
+	}
+
+	public void set(JScrollPane scrollPane, ScrollBarType scrollBarType)
+	{
+		JScrollBar scrollBar = getScrollBar(scrollPane, scrollBarType);
+		if (scrollBar==null) return;
+		ScrollPosition newValues = get(scrollBar);
+		if (newValues==null) return;
+		//System.err.printf("scroll pos (%s): %s -> %s%n", scrollBarType, this, newValues);
+		
+		if (val==0) // start of page -> start of page
+			scrollBar.setValue(val);
+		
+		else if (val+ext >= max) // end of page -> end of page
+			scrollBar.setValue(newValues.max-newValues.ext);
+		
+		else if (val+newValues.ext >= newValues.max) // old val > max -> end of page
+			scrollBar.setValue(newValues.max-newValues.ext);
+		
+		else
+			scrollBar.setValue(val);
+		
+		//System.err.printf("scroll pos (%s): %s -> %s -> %s%n", scrollBarType, this, newValues, get(scrollBar));
+	}
+
+	public static ScrollPosition getVertical  (JScrollPane scrollPane) { return get(scrollPane, ScrollBarType.Vertical  ); }
+	public static ScrollPosition getHorizontal(JScrollPane scrollPane) { return get(scrollPane, ScrollBarType.Horizontal); }
+	public void                  setVertical  (JScrollPane scrollPane) {        set(scrollPane, ScrollBarType.Vertical  ); }
+	public void                  setHorizontal(JScrollPane scrollPane) {        set(scrollPane, ScrollBarType.Horizontal); }
+}
