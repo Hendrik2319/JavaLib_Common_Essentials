92c41f5c30ea5e2b0d9db8ba8f51c7fdbccfbecb
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2024-10-17 19:24:36 +0200

    feat: new GeneralIcons

--- src/net/schwarzbaer/java/lib/gui/GeneralIcons.java
+++ src/net/schwarzbaer/java/lib/gui/GeneralIcons.java
@@ -17,15 +17,17 @@ public final class GeneralIcons
 		Copy, Paste, Cut, Delete,
 		Folder, AddFolder, ReloadFolder,
 		Play, Pause, Stop, Skip2Prev, Skip2Next,
+		Print,Trash,Memory,
 		
 		Muted_Dis, UnMuted_Dis, Up_Dis, Down_Dis, Power_IsOn_Dis, Power_IsOff_Dis,
 		Reload_Dis, ReloadCCW_Dis, Download_Dis, Image_Dis, Save_Dis, Add_Dis,
 		Copy_Dis, Paste_Dis, Cut_Dis, Delete_Dis,
 		Folder_Dis, AddFolder_Dis, ReloadFolder_Dis,
 		Play_Dis, Pause_Dis, Stop_Dis, Skip2Prev_Dis, Skip2Next_Dis,
+		Print_Dis,Trash_Dis,Memory_Dis,
 		;
 		public Icon getIcon() { return iconSource.getCachedIcon(this); }
-		private static IconSource.CachedIcons<GrayCommandIcons> iconSource = IconSource.createCachedIcons(16, 16, 24, "GeneralIcons.GrayCommandIcons.png", GrayCommandIcons.values());
+		private static IconSource.CachedIcons<GrayCommandIcons> iconSource = IconSource.createCachedIcons(16, 16, 27, "GeneralIcons.GrayCommandIcons.png", GrayCommandIcons.values());
 		
 		public enum IconGroup implements GeneralIcons.IconGroup {
 			Muted        (GrayCommandIcons.Muted       , GrayCommandIcons.Muted_Dis       ),
@@ -52,6 +54,9 @@ public final class GeneralIcons
 			Stop         (GrayCommandIcons.Stop        , GrayCommandIcons.Stop_Dis        ),
 			Skip2Prev    (GrayCommandIcons.Skip2Prev   , GrayCommandIcons.Skip2Prev_Dis   ),
 			Skip2Next    (GrayCommandIcons.Skip2Next   , GrayCommandIcons.Skip2Next_Dis   ),
+			Print        (GrayCommandIcons.Print       , GrayCommandIcons.Print_Dis       ),
+			Trash        (GrayCommandIcons.Trash       , GrayCommandIcons.Trash_Dis       ),
+			Memory       (GrayCommandIcons.Memory      , GrayCommandIcons.Memory_Dis      ),
 			;
 			public final GrayCommandIcons  enabledIcon;
 			public final GrayCommandIcons disabledIcon;
